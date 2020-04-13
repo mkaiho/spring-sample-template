ea5b8107ec3c90616010dca3ef91bc0116827e9f
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2020-04-13 13:40:04 +0900

    Add spring project generated by tool

---
+++ src/test/java/com/template/sample/spring/templateapp/SpringSampleTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.sample.spring.templateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
